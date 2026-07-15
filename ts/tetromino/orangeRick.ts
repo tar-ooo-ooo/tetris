34e4d408e8c42d3a72b8fb3ef26329bcf22570d2
林郁盛 <sss890925@gmail.com>
2026-07-15 18:13:03 +0800

    feat: 新增多個 Tetromino 類別並更新 Tetromino 創建邏輯

--- ts/tetromino/orangeRick.ts
+++ ts/tetromino/orangeRick.ts
@@ -1,2 +1,4 @@
 //       ▓▓
 // ▓▓ ▓▓ ▓▓
+
+export class OrangeRick {}
--- ts/tetromino/tetromino.ts
+++ ts/tetromino/tetromino.ts
@@ -1,1 +1,30 @@
-export class Tetromino {}
+import { BlueRick } from "./blueRick";
+import { ClevelandZ } from "./clevelandZ";
+import { Hero } from "./hero";
+import { OrangeRick } from "./orangeRick";
+import { RhodeIslandZ } from "./rhodeIslandZ";
+import { SmashBoy } from "./smashBoy";
+import { Teewee } from "./teewee";
+
+export class Tetromino {
+  public static create = () => {
+    const index: Number = Math.floor(Math.random() * 7);
+
+    switch (index) {
+      case 0:
+        return new BlueRick();
+      case 1:
+        return new ClevelandZ();
+      case 0:
+        return new Hero();
+      case 0:
+        return new OrangeRick();
+      case 0:
+        return new RhodeIslandZ();
+      case 0:
+        return new SmashBoy();
+      case 0:
+        return new Teewee();
+    }
+  };
+}
